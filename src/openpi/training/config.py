71d8f2a111b872a030b2d885071a8ced4b71d563
Gabriele Boscarini <gabrieleboscarini1998@gmail.com>
2026-06-11 12:49:13 +0200

    Update dataset repo_id to gabbobosca namespace

--- src/openpi/training/config.py
+++ src/openpi/training/config.py
@@ -1017,7 +1017,7 @@ _CONFIGS = [
         name="pi05_ur5",
         model=pi0_config.Pi0Config(pi05=True, paligemma_variant="gemma_2b_lora", action_expert_variant="gemma_300m_lora"),
         data=LeRobotUR5DataConfig(
-            repo_id="gabrieleboscarini/mir_ur_pick_place",
+            repo_id="gabbobosca/mir_ur_pick_place",
             base_config=DataConfig(prompt_from_task=True),
         ),
         weight_loader=weight_loaders.CheckpointWeightLoader("gs://openpi-assets/checkpoints/pi05_base/params"),
